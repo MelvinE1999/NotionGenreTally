2b22e0ddae916a643f9bf91b2e929e892f4ce59f
MelvinE1999 <73800751+MelvinE1999@users.noreply.github.com>
2021-10-09 20:29:43 -0700

    Implemented the GUI for the program

--- venv/Main.py
+++ venv/Main.py
@@ -2,26 +2,27 @@
 # if for the pie chart want to select by check boxes
 # any features they want it to have for now
 
-#if notionpy doesnt work could have her export to csv and read from there
-import info
+# if notionpy doesnt work could have her export to csv and read from there
+# import info     # unneded for now until notionpy is fixed
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from notion.client import NotionClient
 import requests
 from tkinter import *
 from tkinter.filedialog import askopenfile
-from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 from tkinter import ttk
 import tkinter as tk
 
-
 collection = {}
 labels = []
 sizes = []
+hasDataBeenEntered = False
 
 dataWindow = tk.Tk()
 cols = ('Rank', 'Genre', 'Quantity')
 dataTable = ttk.Treeview(dataWindow, columns=cols, show='headings')
+
 
 def test_internet():
     url = "http://www.google.com"
@@ -51,10 +52,8 @@ def quantity_Order():
 
     dataTable.delete(*dataTable.get_children())
 
-    dataInOrder = dict(sorted(data.items(), key=lambda item: item[1]))  # sorts by value  in increasing order
+    dataInOrder = dict(sorted(collection.items(), key=lambda item: item[1]))  # sorts by value  in increasing order
     for key in reversed(dataInOrder.keys()):
-        labels.append(key)
-        sizes.append(dataInOrder[key])
         tempGenre = str(key)
         tempQuantity = str(dataInOrder[key])
         dataTable.insert("", "end", values=(counter, tempGenre, tempQuantity))
@@ -66,46 +65,71 @@ def abcOrder():
 
     dataTable.delete(*dataTable.get_children())
 
-    for key in sorted(data.keys()):
-        labels.append(key)
-        sizes.append(data[key])
+    for key in sorted(collection.keys()):
         tempGenre = str(key)
-        tempQuantity = str(data[key])
+        tempQuantity = str(collection[key])
         dataTable.insert("", "end", values=(counter, tempGenre, tempQuantity))
         counter = counter + 1
 
 
 def display_chart():
-    # code below copied for testing
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, labels=labels, shadow=True, startangle=90)
-    ax1.axis("equal")
+    global hasDataBeenEntered
 
-    plt.show()
+    pieChartWindow = Toplevel()
+    pieChartWindow.title("Pie Chart")
+
+    figure = plt.figure(figsize=(6, 6), dpi=100)
+    figure.set_size_inches(10, 8)
+
+    if not hasDataBeenEntered:
+        for key in sorted(collection.keys()):
+            labels.append(key)
+            sizes.append(collection[key])
+        hasDataBeenEntered = True
+
+    plt.pie(sizes, labels=labels, shadow=True, startangle=90)
+    plt.axis("equal")
+
+    graph = FigureCanvasTkAgg(figure, pieChartWindow)
+    graph.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
 
 
 def get_comma_number_of_genre(listOfHeader):
     commaCounter = 0
-    if(len(listOfHeader) == 0): return  # gaurdian clause
-    listOfHeader = listOfHeader[1:] # gets rid of the formatting at the beginning of the file
+    if (len(listOfHeader) == 0): return  # gaurdian clause
+    listOfHeader = listOfHeader[1:]  # gets rid of the formatting at the beginning of the file
     headers = listOfHeader.split(',')
     for header in headers:
-        if header == "Column" or header == "column":
+        if header == "Tags" or header == "tags":
             return commaCounter
         else:
             commaCounter = commaCounter + 1
 
 
+def get_genres(line, targetedIndex):
+    parenChecker = False
+    entries = line.split(',')
+    while True:
+        if entries[targetedIndex] == '':
+            return
+        elif entries[targetedIndex].startswith('\"'):
+            parenChecker = True
+            entries[targetedIndex] = entries[targetedIndex][1:]
+        elif entries[targetedIndex].endswith('\"'):
+            parenChecker = False
+            entries[targetedIndex] = entries[targetedIndex][0:
+                                                len(entries[targetedIndex]) - 1]
+        tempValue = entries[targetedIndex]
+        if tempValue in collection.keys():
+            collection[tempValue] = collection[tempValue] + 1
+        else:
+            collection[tempValue] = 1
 
-def get_genres(line, commaNumber):
-    for char in line:
-        if(commaNumber == commaCount):
-            if(char == '\"'):
-                continue
-        # add a checker for open and closed quotes
-        # store data in the setup dictionaries
-        if(char == ','):
-            commaCount = commaCount + 1
+        if parenChecker == False:
+            return
+
+        targetedIndex = targetedIndex + 1
 
 
 def get_data_from_file():
@@ -114,20 +138,14 @@ def get_data_from_file():
     # need to add parsing below
     header = next(file)
     commaNumber = get_comma_number_of_genre(header)
-    for line in file.readlines(): # loop for the rest of the file minus header
+    for line in file.readlines():  # loop for the rest of the file minus header
         get_genres(line, commaNumber)
 
 
-def close():
-    dataWindow.wm_withdraw() # closes window might be helpful
-
-
 def run():
-    displayChart = True
-    choice = ''
-
     # test_internet()    not needed until notionpy fixed
     # collect_data_from_notion()  notionpy is not working at the moment
+    get_data_from_file()
 
     for col in cols:
         dataTable.heading(col, text=col)
@@ -137,19 +155,11 @@ def run():
                                 command=abcOrder).grid(row=4, column=0)
     showScoresInQuantityDescending = tk.Button(dataWindow, text="Descending order",
                                                width=15, command=quantity_Order).grid(row=4, column=1)
-    closeButton = tk.Button(dataWindow, text="close",width=15, command=close).grid(row=4, column=2)
+    displayChart = tk.Button(dataWindow, text="Display Chart", width=15,
+                             command=display_chart).grid(row=4, column=2)
 
     dataWindow.mainloop()
 
-    while choice.lower() != 'yes' or choice.lower() != 'no':
-        choice = input("Would you like to display the pie chart? (yes or no)\n")
-        if choice.lower == 'no':
-            displayChart = False
-
-    if displayChart:
-        display_chart()
-
 
 if __name__ == '__main__':
-    #run()
-    get_data_from_file()
+    run()
